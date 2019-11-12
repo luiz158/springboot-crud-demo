33db0710f161e7d33cb13d336afd246dd50027a5
losete <losetazo@gmail.com>
2019-11-12 13:57:34 +0100

    fix/solve_dependency_problems (#1)
    * Updated spring version
    
    * Delete application.properties
    
    * Updated gitignore + Added application.properties
    
    * Added maven.compiler properties
    
    * Update spring plugin version
    
    Modify test framework due implementation changes on plugin

--- src/test/java/com/ensat/WebAppApplicationTests.java
+++ src/test/java/com/ensat/WebAppApplicationTests.java
@@ -3,11 +3,11 @@ package com.ensat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = SpringBootWebApplication.class)
+@SpringBootTest(classes = SpringBootWebApplication.class)
 @WebAppConfiguration
 public class WebAppApplicationTests {
 
