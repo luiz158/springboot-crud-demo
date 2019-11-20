91fd9089a8eddad2f17a0469152343f8ca26fc25
losete <losetazo@gmail.com>
2019-11-20 09:22:05 +0100

    enhancement/add_spotbugs (#7)
    * Added spotbugs plugin + Added exclude filter
    
    * Moved spotbugs execution to verify phase

--- src/main/java/com/ensat/controllers/IndexController.java
+++ src/main/java/com/ensat/controllers/IndexController.java
@@ -2,6 +2,7 @@ package com.ensat.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * Homepage controller.
